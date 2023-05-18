654715cc71dcaad215f762bb77a4131ee0650105
Ismatulla Mansurov <47342870+sapoepsilon@users.noreply.github.com>
2023-05-18 13:29:35 -0600

    added all the classes

--- src/DessertItem.java
+++ src/DessertItem.java
@@ -1,2 +1,4 @@
+
+// Do not change class names for the JUnit tests. The names are used for autograding
 public class DessertItem {
 }
---
+++ src/IceCreamTest.java
@@ -0,0 +1,7 @@
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+// Do not change class names for the JUnit tests. The names are used for autograding
+public class IceCreamTest {
+}
